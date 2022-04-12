e56751a3af7e4d753a619047265cc3dcaa2ff494
GPG0709 <guillermo.puga@kellogg.northwestern.edu>
2022-04-12 05:50:08 +0000

    Airbnb analysis

--- practice-5.sql
+++ practice-5.sql
@@ -12,4 +12,7 @@
 -- | Private room in townhouse           | 3        |
 -- +-------------------------------------+----------+
 
-
+SELECT Property_type, COUNT(*)
+FROM listings
+WHERE neighborhood = "Hyde Park"
+GROUP BY Property_type;
